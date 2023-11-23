0c0bc4997c5233f1c1122410ea242d78e945f282
Almontheir <141272895+Almontheir@users.noreply.github.com>
2023-11-23 16:35:59 +0200

    Update main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,6 +2,7 @@
 
 #define _MAIN_H_
 
+
 int _putchar(char c);
 
 unsigned int binary_to_uint(const char *b);
@@ -10,12 +11,13 @@ void print_binary(unsigned long int n);
 
 int get_bit(unsigned long int n, unsigned int index);
 
-int clear_bit(unsigned long int *n, unsigned int index);
-
 int set_bit(unsigned long int *n, unsigned int index);
+
+int clear_bit(unsigned long int *n, unsigned int index);
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 
 int get_endianness(void);
+
 
 #endif
